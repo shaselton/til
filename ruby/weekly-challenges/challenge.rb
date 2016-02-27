ff588a6fdf90557e03c1b8798294bf5e78cc7a9e
Scott Haselton <shaselton@gmail.com>
2016-02-27 11:12:08 -0800

    fixed minor conditional error

--- ruby/weekly-challenges/challenge.rb
+++ ruby/weekly-challenges/challenge.rb
@@ -3,7 +3,7 @@
 #
 a = ARGV[0].split(',') || []
 b = a.each_with_object({even: {}, invalid: []}).with_index do |(element, dictionary), index|
-  if index % 2 == 0 && dictionary[element].nil? && !dictionary[:invalid].include?(element)
+  if index % 2 == 0 && dictionary[:even][element].nil? && !dictionary[:invalid].include?(element)
     dictionary[:even][element] = index
   elsif index % 2 == 1 
     dictionary[:even].delete(element) if dictionary[:even][element]
