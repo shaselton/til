622e7af4380d69cc99ae2311dee8d4b347c9d18b
Scott Haselton <shaselton@gmail.com>
2016-02-27 11:10:36 -0800

    basic algo started

---
+++ ruby/weekly-challenges/challenge.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+#
+#
+a = ARGV[0].split(',') || []
+b = a.each_with_object({even: {}, invalid: []}).with_index do |(element, dictionary), index|
+  if index % 2 == 0 && dictionary[element].nil? && !dictionary[:invalid].include?(element)
+    dictionary[:even][element] = index
+  elsif index % 2 == 1 
+    dictionary[:even].delete(element) if dictionary[:even][element]
+    dictionary[:invalid] << element if !dictionary[:invalid].include?(element)
+  end    
+end
+key, value = b[:even].first
+puts "value #{key} was found at index #{value}" if key
